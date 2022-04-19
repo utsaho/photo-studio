b76a7ffdfa7654eaf8618f2cbfaba011b96ed410
utsho <utsho111099@gmail.com>
2022-04-19 22:34:18 +0600

    about me added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,8 +7,10 @@ const About = () => {
             <div className='d-flex justify-content-center align-items-center mt-5'>
                 <img className='rounded-3' src="https://scontent.fdac3-1.fna.fbcdn.net/v/t1.6435-9/121103997_826271984789611_4756420523410734231_n.jpg?stp=cp0_dst-jpg_e15_q65_s320x320&_nc_cat=111&ccb=1-5&_nc_sid=dd9801&efg=eyJpIjoiYiJ9&_nc_eui2=AeHi1e_XDMHr8ZwfIXmqs1bhemV724CCme96ZXvbgIKZ7wJgBnABLOtd3G0eU0tBmg6ZQOtC9WrIV58ojwmqnxHq&_nc_ohc=H7YmSYWQ720AX8j19Kb&_nc_ht=scontent.fdac3-1.fna&oh=00_AT_dMAK1BlQKe9Up44g1ahutZXnd4Yun9SlzAPF-dmcr2Q&oe=628482EE" alt="" />
             </div>
-            <div className='mt-5'>
-                <p style={{ fontFamily: 'consolas' }}>hello world</p>
+            <div className='mt-5 container'>
+                <p style={{ fontFamily: 'consolas' }}> <span className='display-6'>Hello!!!</span>Good to see you here. I am pipul boddyo. My nearby persons also called me 'utsho'. I want to do something better what would help people and I can enjoy this journey. You know web is the biggest platform to connect peoples and make the world more small. <br />
+                    So my goal is to work with bigger company and make the web more easier. I am doing hard work to achieve this goal. I spend most of time on web-development beside my academic studies. I also try to continue competitive programming </p>
+                <h3 className='text-center'>Thank you</h3>
             </div>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,13 +22,6 @@ const Header = () => {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                             <Nav.Link as={Link} to='/gallery'>Gallery</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="/about">About</Nav.Link>
